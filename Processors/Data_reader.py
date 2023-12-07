132bacffe576d199d5d3537d78458836bca3fc0f
Jan-HSLU <145695343+Jan-HSLU@users.noreply.github.com>
2023-12-07 08:56:17 +0100

    Add files via upload

---
+++ Processors/Data_reader.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import ifcopenshell
+import Helpers.Data_Helpers as hlp
+
+def read_data():
+    st.set_page_config()
+    st.sidebar.title("Willkommen zur Baueingabevorprüfung!")
+
+    uploaded_files = st.sidebar.file_uploader("Bitte laden Sie die drei Dateien hoch", accept_multiple_files=True, type=['ifc'])
+    VOM_data, NUM_data, BAM_data = None, None, None
+    ifcpaths = None 
+
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+            if uploaded_file.name.endswith("_VOM.ifc"):
+                VOM_data = ifcopenshell.file.from_string(uploaded_file.getvalue().decode("utf-8"))
+            elif uploaded_file.name.endswith("_NUM.ifc"):
+                NUM_data = ifcopenshell.file.from_string(uploaded_file.getvalue().decode("utf-8"))
+            elif uploaded_file.name.endswith("_BAM.ifc"):
+                BAM_data = ifcopenshell.file.from_string(uploaded_file.getvalue().decode("utf-8"))
+
+        if None not in [VOM_data, NUM_data, BAM_data]:
+            ifcpaths = hlp.IfcPaths(VOM_data, NUM_data, BAM_data)
+        else:
+            st.sidebar.error("Es müssen genau drei Modelle eingelesen werden! Achte auf Schreibfehler.")
+
+    link_text = "Für Beispieldaten hier klicken"
+    url = "https://github.com/Jan-HSLU/Baueingabe.git"
+    st.markdown(f"[{link_text}]({url})")
+
+    return ifcpaths
+
+
+if __name__ == "__main__":
+    paths_list = read_data()
